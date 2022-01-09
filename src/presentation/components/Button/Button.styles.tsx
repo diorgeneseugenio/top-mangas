93d508db3937df2e1b5104993b835116823d8743
Diórgenes Eugênio <diorgeneseugenio@Diorgeness-MacBook-Pro.local>
2022-01-09 00:47:59 -0300

    feat: create component button

---
+++ src/presentation/components/Button/Button.styles.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+import tw from 'twin.macro'
+
+export interface IButtonStyledProps {
+  variant: 'primary' | 'secondary' | 'disabled'
+}
+
+export const ButtonStyled = styled.button(({ variant }: IButtonStyledProps) => [
+  tw`px-6 py-3 rounded-lg border-b-4 hover:border-b-0 transition-all ease-in-out duration-100`,
+  variant === 'primary' && tw`bg-red-600 border-b-red-500 text-white`,
+  variant === 'secondary' && tw`bg-yellow-500 border-b-yellow-400 text-white`,
+  variant === 'disabled' && tw`bg-gray-200`
+])
---
+++ src/presentation/components/Button/Button.tsx
@@ -0,0 +1,12 @@
+import React, { ReactChild } from 'react'
+import { ButtonStyled, IButtonStyledProps } from './Button.styles'
+
+interface OwnProps extends IButtonStyledProps {
+  children: ReactChild
+}
+
+export const Button = (props: OwnProps) => {
+  const { children } = props
+
+  return <ButtonStyled {...props}>{children}</ButtonStyled>
+}
